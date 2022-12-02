7ec06290f1575e4e6e97912adb8153a9465c7278
jerome <2213540@slu.edu.ph>
2022-12-03 00:13:27 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+"""
+Asks for the values
+"""
+def input_values():
+    equation = ""
+    initial_guess = []
+    return equation, initial_guess
+
+
+def parabolic_interpolation(equation, initial_guess):
+    x = 0
+    x_values = initial_guess.copy()
+    tempx_values = []
+    fx_values = []
+    return x
+
+
+def print_values(tempx_values, fx_values):
+
+
+
+if __name__ == '__main__':
+    equation, initial_guess = input_values()
+    x = parabolic_interpolation(equation, initial_guess)
+
